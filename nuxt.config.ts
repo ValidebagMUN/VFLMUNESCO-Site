b7bf883f8d004b36fdb51677c27eb6831d80b928
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2024-04-07 00:08:28 +0300

    feat: add typed router

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/color-mode', '@nuxtjs/tailwindcss', '@nuxt/image'],
+  modules: [
+    '@nuxtjs/color-mode',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+    "nuxt-typed-router"
+  ],
   colorMode: {
     classSuffix: '',
   },
