049da99b6dede83f49673a23d5e16d02a64fd05f
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2024-04-07 00:17:22 +0300

    feat: add new page menus

--- components/Header.vue
+++ components/Header.vue
@@ -22,30 +22,42 @@
                     <li><a>Item 3</a></li>
                 </ul>
             </div>
-            <NuxtImg src="/vmun-light.svg" alt="ValidebagMUN logo" class="h-16 ml-1" v-show="isDark" />
-            <NuxtImg src="/vmun-dark.svg" alt="ValidebagMUN logo" class="h-16 ml-1" v-show="!isDark" />
-            <NuxtLink class="btn btn-ghost text-2xl mx-0 px-1.5" to="/">ValidebağMUN</NuxtLink>
+
+            <NuxtImg src="/vmun-light.svg" alt="ValidebagMUN logo" class="h-16 ml-1 hover:animate-pulse" v-show="isDark" />
+            <NuxtImg src="/vmun-dark.svg" alt="ValidebagMUN logo" class="h-16 ml-1 hover:animate-pulse" v-show="!isDark" />
+            <NuxtLink id="logo" class="btn btn-ghost text-2xl mx-0 px-1.5" to="/">ValidebağMUN</NuxtLink>
         </div>
         <div class="hidden navbar-center lg:flex">
             <ul class="px-1 menu menu-horizontal">
                 <div class="dropdown dropdown-hover">
-                    <div tabindex="0" role="button" class="m-1 btn btn-ghost">Hover</div>
-                    <ul tabindex="0" class="dropdown-content z-[1] menu p-2 bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li><a>Item 2</a></li>
+                    <div tabindex="0" role="button" class="m-1 mb-0 text-lg btn btn-ghost no-animation">About</div>
+                    <ul tabindex="0" class="dropdown-content z-[1] menu p-2 bg-base-100 rounded-box w-32 font-semibold">
+                        <li><NuxtLink to="/conference/venue">Venue</NuxtLink></li>
+                        <li><NuxtLink to="/conference/schedule">Schedule</NuxtLink></li>
                     </ul>
                 </div>
                 <div class="dropdown dropdown-hover">
-                    <div tabindex="0" role="button" class="m-1 btn btn-ghost">Hover</div>
-                    <ul tabindex="0" class="dropdown-content z-[1] menu p-2 bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li><a>Item 2</a></li>
+                    <div tabindex="0" role="button" class="m-1 mb-0 text-lg btn btn-ghost no-animation">Our Team</div>
+                    <ul tabindex="0" class="dropdown-content z-[1] menu p-2 bg-base-100 rounded-box w-32 font-semibold">
+                        <li><NuxtLink to="/team/secretariat">Secretariat</NuxtLink></li>
+                        <li><NuxtLink to="/team/directorate">Directorate</NuxtLink></li>
                     </ul>
                 </div>
+                <div class="dropdown dropdown-hover">
+                    <div tabindex="0" role="button" class="m-1 mb-0 text-lg btn btn-ghost no-animation">Committees</div>
+                    <ul tabindex="0" class="dropdown-content z-[1] menu p-2 bg-base-100 rounded-box w-36 font-semibold">
+                        <li><NuxtLink to="/committees/unodc">UNODC: CND</NuxtLink></li>
+                        <li><NuxtLink to="/committees/ecosoc">ECOSOC</NuxtLink></li>
+                        <li><NuxtLink to="/committees/unga">UNGA</NuxtLink></li>
+                        <li><NuxtLink to="/committees/unsc">UNSC</NuxtLink></li>
+                    </ul>
+                </div>
+                <NuxtLink tabindex="0" role="button" class="m-1 text-lg btn btn-ghost" to="/faq">FAQ</NuxtLink>
+                <NuxtLink tabindex="0" role="button" class="m-1 text-lg btn btn-ghost" to="/contact">Contact</NuxtLink>
             </ul>
         </div>
         <div class="mr-1 navbar-end">
-            <NuxtLink to="/apply" class="text-secondary btn btn-ghost bg-primary dark:bg-primary dark:text-secondary hover:bg-accent dark:hover:bg-accent-content">Apply Now</NuxtLink>
+            <NuxtLink id="applyButton" tabindex="0" role="button" to="/apply" class="text-secondary btn btn-ghost bg-primary dark:bg-accent-content dark:text-secondary hover:bg-accent">Apply Now</NuxtLink>
         </div>
     </div>
 </template>
@@ -59,4 +71,18 @@ const props = defineProps({
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.btn-ghost {
+    @apply dark:hover:bg-accent-content
+}
+.dropdown-content {
+    @apply dark:bg-accent-content
+
+}
+#logo {
+    @apply dark:hover:bg-inherit
+}
+#applyButton {
+    @apply dark:hover:bg-primary
+}
+</style>
