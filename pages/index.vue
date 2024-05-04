c978637233746e15c3385b32237b01dde75fff08
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2024-05-04 22:00:22 +0300

    feat: add sg letter

--- pages/index.vue
+++ pages/index.vue
@@ -34,8 +34,15 @@
                 </div>
             </div>
         </div>
-        <div class="hero">
-            
+        <div class="min-h-[80vh] py-10 pt-20 mx-20 text-neutral dark:text-neutral-content">
+            <h2 class="text-3xl font-bold">Letter from the Secretary-General</h2>
+            <div class="m-0 my-3 divider divider-primary dark:divider-info"></div>
+            <h3 class="mb-2 text-2xl font-bold">Dear Participants,</h3>
+            <p class="text-xl">
+            I as the Secretary-General, welcome you all to the first edition of Validebağ Science High School Model United Nations Conference. With a dedicated vision to portray and reflect the dynamics of the United Nations system and global politics, and a bold mission to ensure a remarkable experience to all of our participants; we are in a long journey of hardwork and dedication. In accordance with our academic preferences and organizational perfectionism we wish to create a nearly one-to-one diplomatic environment for students just like us.
+            <br><br>We see it as a priority to equip you with the necessary awareness as we prepare to take stage and become the leaders of next generations. With ValidebağMUN’24, we tried to achieve this by emphasizing the words United Nations in the general concept of MUNs. Nowadays, most MUNs may lack the sufficient academic ambition but we can guarantee, we possess this much-needed academic ambition and an avant-garde vision for our future. So, once again; I welcome you all to an unmatched avant-garde diplomatic and political experience.
+            </p>
+            <h3 class="mt-4 text-2xl font-bold">Hakan Rüzgar Kocaoğlu <br> Secretary-General</h3>
         </div>
     </div>
 </template>
