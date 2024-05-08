a2543cf76b2942f9af2739d2eff47087d04c60ec
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2024-05-08 16:34:22 +0300

    fix: cumulative layout shift in committee component

--- components/Committee.vue
+++ components/Committee.vue
@@ -5,7 +5,12 @@
     </Head>
     <div class="pt-8 hero text-neutral dark:text-neutral-content">
         <div class="flex-col hero-content lg:flex-row-reverse">
-            <NuxtImg preload quality="80" v-if="image" :src="image" class="max-w-sm -mt-20 rounded-lg shadow-2xl bg-base-100" />
+            <div v-if="image">
+                <NuxtImg preload v-show="isLoaded"
+                    @load="() => { console.log('isLoaded'); return isLoaded = true }" :src="image"
+                    class="max-w-sm rounded-lg shadow-2xl lg:-mt-20 bg-base-100" />
+                <div v-show="!isLoaded" class="rounded-lg shadow-2xl max-w-[24rem] min-w-[15.625rem] aspect-square skeleton lg:-mt-20" />
+            </div>
             <div>
                 <h1 class="mb-3 text-5xl font-bold">{{ slug.includes("CC") ? slug : slug.toUpperCase() }}</h1>
                 <h2 class="text-3xl">{{ name }}</h2>
@@ -18,11 +23,13 @@
                 <div v-if="asg || chair" class="divider divider-neutral dark:divider-primary"></div>
                 <div class="my-4 text-xl">
                     <h3 v-if="asg"><span class="font-semibold">Assistant-Secretary-General:</span> {{ asg }}</h3>
-                    <h3 v-if="chair"><span class="font-semibold">{{ chair.split(' - ').length > 1 ? 'Presidency: ' : 'President: ' }}</span>{{ chair }}</h3>
+                    <h3 v-if="chair"><span class="font-semibold">{{ chair.split(' - ').length > 1 ? 'Presidency: ' :
+                            'President: ' }}</span>{{ chair }}</h3>
                 </div>
                 <div v-if="rop || sg" class="divider divider-neutral dark:divider-primary"></div>
                 <div class="pb-6">
-                    <NuxtLink target="_blank" v-if="rop" :to="rop" class="mr-5 btn btn-primary">Rules of Procedure</NuxtLink>
+                    <NuxtLink target="_blank" v-if="rop" :to="rop" class="mr-5 btn btn-primary">Rules of Procedure
+                    </NuxtLink>
                     <NuxtLink target="_blank" v-if="sg" :to="sg" class="mr-5 btn btn-primary">Study Guide</NuxtLink>
                 </div>
             </div>
@@ -31,6 +38,7 @@
 </template>
 
 <script setup lang="ts">
+const isLoaded = ref(false)
 defineProps({
     slug: {
         type: String,
@@ -73,6 +81,6 @@ defineProps({
 
 <style scoped>
 .btn {
-    @apply text-lg text-neutral-content 
+    @apply text-lg text-neutral-content
 }
 </style>
