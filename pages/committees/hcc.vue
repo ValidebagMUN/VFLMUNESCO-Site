2a6525a30de56b14bed7fea86e717e30c870c983
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2024-05-08 16:34:47 +0300

    chore: some crisis chairboards

--- pages/committees/hcc.vue
+++ pages/committees/hcc.vue
@@ -12,7 +12,8 @@ const committee = {
         'The Reign of the King of Yunnan: Long Yun',
     ],
     text: 'War. War never changes.\nSince the dawn of human kind, when our ancestors first discovered the killing power of rock and bone, blood has been spilled in the name of everything: from God to justice to simple, psychotic rage. In the turbulent years of interbellum, after years of armed conflicts, the destructive nature of man could sustain itself no longer. China was plunged into an abyss of chaos and death. During all these anarchy, a man rose from a distant province covered by a ruthless wilderness and mountains so high that they block the skies; Long Yun. The man quickly proved himself to be a leader, controlling all of Yunnan and organizing an army in the name of the Nationalist Party. The King of Yunnan was his name, he marched through a rift of destruction and death with a bold ambition. Long Yun is much aware that this is a point of no return. It was here he was born. It is here he will die. Because, in this realm no one ever enters, and no one ever leaves.',
-    image: '/img/hcc.png'
+    image: '/img/hcc.png',
+    asg: 'Mehmet Tuğra Coşkun'
 }
 </script>
 
--- pages/committees/jcc.vue
+++ pages/committees/jcc.vue
@@ -12,7 +12,8 @@ const committee = {
         'Turkish Local Elections of 1930'
     ],
     text: 'The 1930 Turkish Local Elections marked the first multi-party election in modern Turkish political history. The ruling Republican People’s Party (CHF) and the newly established Liberal Republic Party (SCF) competed against each other in a not-so-competitive manner. The SCF was created by a close friend of the Reisicümhur, Ali Fethi Okyar and the party itself was nominally liberal despite de facto turning into an anti-CHF, anti-Atatürk political organization. Their success was limited just like their lifespan, despite Atatürk even making his sister a member of the SCF, the multi-party experiment failed to compete against the violent dynamics of a still hot revolution and a uniparty establishment unwantedly headed by the Reisicümhur himself.\n\nThe CHF, at that time, was one of the reflections of a political trinity: State, President, and the Party. Any competition against the CHF was seen as a rejection of the Kemalist revolutions and principles on which the state was founded on. The State was the state and the government of the Reisicümhur Mustafa Kemal Atatürk. As the founder of a young and ambitious republic, Atatürk was not challenged by the political elite; any opposition could be labeled as traitorous; in all this silence, Atatürk challenged himself, he had already tried once with the Progressive Republic Party (TCF) whose existence and importance was shadowed by the unfortunate attempted assassination in İzmir and an evil plot against the State in the Eastern Provinces. Atatürk sought to create opposition to his party again with the SCF and preferred to remain neutral in an attempt to break the chains of the political trinity. Will the chain be broken, or will the chains put an end to multiparty experiments of the Atatürk Era?',
-    image: '/img/jcc.png'
+    image: '/img/jcc.png',
+    chair: 'Berfin Kılınçaslan - Yaren Naz Yıldırım'
 }
 </script>
 
