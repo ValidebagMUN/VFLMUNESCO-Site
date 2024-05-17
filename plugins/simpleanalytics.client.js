77b6dd7aa46e28a91b8c11d0818b5634a698fd3e
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2024-05-17 09:16:36 +0300

    sa fix

--- plugins/simpleanalytics.client.js
+++ plugins/simpleanalytics.client.js
@@ -1,7 +1,7 @@
 import SimpleAnalytics from "simple-analytics-vue"
 
 export default defineNuxtPlugin((nuxtApp) => {
-  console.log(nuxtApp.$config.public);
+  console.log(nuxtApp.$config.public.VERCEL_ENV);
   nuxtApp.vueApp.use(SimpleAnalytics, {
     skip: nuxtApp.$config.public.VERCEL_ENV !== "production",
     // domain: "sa.validebagmun.org"
