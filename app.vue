dc6aa4312d79da875acb4d71c14eb2fd0073ed27
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2025-03-31 16:01:57 +0300

    initial refactor commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+    <button class="dark:text-red-500 text-amber-300">AHMET KAYA</button>
+  </div>
+</template>
--- server/api/maintenance.ts
+++
@@ -1,4 +0,0 @@
-export default defineEventHandler(async (event) => {
-    const isMaintenance = JSON.parse(useRuntimeConfig().maintenance)
-    return isMaintenance
-})
