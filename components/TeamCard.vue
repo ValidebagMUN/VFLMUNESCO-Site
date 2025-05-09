0faef110048fba0218f0ef1631839468eaaa5358
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2025-05-09 20:30:53 +0300

    feat: the uncanny valley

--- components/TeamCard.vue
+++ components/TeamCard.vue
@@ -2,7 +2,8 @@
     <div class="card shadow-xl w-80 mx-4 mt-6 mb-10 bg-base-100" key="name">
         <figure>
             <NuxtImg v-show="isLoaded" @load="() => { console.log('isLoaded'); return isLoaded = true }" :src="img" :alt="title" />
-            <div v-show="!isLoaded" class="rounded-b-none shadow-2xl skeleton min-w-80 min-h-80" />
+            <!-- <div v-show="!isLoaded" class="rounded-b-none shadow-2xl skeleton min-w-80 min-h-80" /> -->
+            <NuxtImg v-show="!isLoaded" class="rounded-b-none shadow-2xl min-w-80 min-h-80" src="/img/team/def.png"/>
         </figure>
         <div class="card-body">
             <h2 class="card-title">{{  name }}</h2>
