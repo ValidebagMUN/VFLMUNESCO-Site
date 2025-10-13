06e34f3df8b0876479847fb33dd95c6ff8e4b5b9
HexaBinary <egeanakli+github@pm.me>
2025-10-13 17:11:59 +0300

    simple analytics setup

--- components/SimpleAnalyticsBadge.vue
+++ components/SimpleAnalyticsBadge.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="mt-2">
-        <a aria-label="See the analytics data of our site." href="https://simpleanalytics.com/validebagmun.org?utm_source=validebagmun.org&utm_content=badge"
+        <a aria-label="See the analytics data of our site." href="https://simpleanalytics.com/munesco.validebagmun.org?utm_source=validebagmun.org&utm_content=badge"
             referrerpolicy="origin" target="_blank">
             <picture>
-                <img src="https://simpleanalyticsbadges.com/validebagmun.org?mode=dark&background=18181b&text=ffffff" loading="lazy" referrerpolicy="no-referrer" crossorigin="anonymous" alt="SimpleAnalytics Badge" v-if="isDark"/>
-                <img src="https://simpleanalyticsbadges.com/validebagmun.org?mode=light&text=0D1B2A&background=f8f8f8" loading="lazy"
+                <img src="https://simpleanalyticsbadges.com/munesco.validebagmun.org?mode=dark&background=18181b&text=ffffff" loading="lazy" referrerpolicy="no-referrer" crossorigin="anonymous" alt="SimpleAnalytics Badge" v-if="isDark"/>
+                <img src="https://simpleanalyticsbadges.com/munesco.validebagmun.org?mode=light&text=0D1B2A&background=f8f8f8" loading="lazy"
                     referrerpolicy="no-referrer" crossorigin="anonymous" alt="SimpleAnalytics Badge" v-if="!isDark"/>
             </picture>
         </a>
